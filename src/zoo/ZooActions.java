2e40bdcc211048b8a483c575a07f4b8cd268eacc
glebtcivie <gleb@tcivie.com>
2022-04-02 16:44:26 +0300

    Added zooActions methods

--- src/zoo/ZooActions.java
+++ src/zoo/ZooActions.java
@@ -7,7 +7,25 @@ import mobility.Point;
 
 public abstract class ZooActions{
 
+    /**
+     * Eating method for animal and its food
+     * @param animal The animal that should eat
+     * @param food The food that the animal will try to eat
+     * @return True if the animal ate the food / False otherwise
+     */
     public static boolean eat(Object animal, IEdible food) {
-        return ((Animal)animal).eat(food);
+        return ((Animal) animal).eat(food);
     }
+
+    /**
+     * Method that will move the animal from one place to another
+     * @param animal The animal that we need to move
+     * @param point To where we want to move the animal
+     * @return True if the animal has moved / False otherwise
+     */
+    public boolean move(Object animal, Point point) {
+        return ((Animal) animal).move(point) > 0;
+    }
+
+
 }
