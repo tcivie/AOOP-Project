12b0b246c119e702d5a28c3a8840506376245611
glebtcivie <gleb@tcivie.com>
2022-04-19 17:21:33 +0300

    Added bottom pane with the action buttons ( Without the actions and the listeners )

--- src/graphics/ZooFrame.java
+++ src/graphics/ZooFrame.java
@@ -14,20 +14,40 @@ public class ZooFrame extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("AOOP Assignment 2 - Zoo"); // Create the frame
         frame.setLayout(new BorderLayout()); // Set border layout
+
         frame.setJMenuBar(setMenuBar()); // add the menu to the frame
+        frame.add(new ZooPanel()); // Add zoo panel to the frame
+
+        frame.add(actionButtons(),BorderLayout.SOUTH); // Create new buttons panel and add it to the bottom
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Sets default close operation
         frame.setSize(300, 200); // Set the frame size
         frame.setVisible(true);
         frame.setLocationRelativeTo(null); // Centers the frame
-//        frame.pack();
+        frame.pack();
     }
 
+    /**
+     * Creates new navigation bar with JMenuBar with the following hierarchy:
+     *
+     * NavBar/
+     * ├─ File/
+     * │  ├─ Exit
+     * ├─ Background/
+     * │  ├─ Image
+     * │  ├─ Green
+     * │  ├─ None
+     * ├─ Help/
+     * │  ├─ Help
+     *
+     * @return new JMenu bar with the fields and the actions
+     */
     private static JMenuBar setMenuBar() {
         JMenuBar menuBar = new JMenuBar(); // Create menu bar
 
         JMenu menuFile = new JMenu("File"); // Create section File
         JMenuItem menuItemExit = new JMenuItem("Exit"); // Add exit item
+
 
         menuItemExit.addActionListener(new ActionListener() { // Add action to exit item
             @Override
@@ -74,7 +94,7 @@ public class ZooFrame extends JFrame {
         menuItemHelp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO: Add help popup
+                JOptionPane.showMessageDialog(getFrames()[0],"Home Work 2\nGUI"); // Show popup with the message
             }
         });
         menuHelp.add(menuItemHelp);
@@ -83,5 +103,26 @@ public class ZooFrame extends JFrame {
         menuBar.add(menuBackground);
         menuBar.add(menuHelp);
         return menuBar;
+    }
+
+    private static JPanel actionButtons() {
+        JPanel buttons = new JPanel();
+
+        JButton addAnimalButton = new JButton("Add Animal");
+        JButton moveAnimalButton = new JButton("Move Animal");
+        JButton clearButton = new JButton("Clear");
+        JButton foodButton = new JButton("Food");
+        JButton infoButton = new JButton("Info");
+        JButton exitButton = new JButton("Exit");
+        //TODO: Add actions to the buttons
+
+        buttons.add(addAnimalButton);
+        buttons.add(moveAnimalButton);
+        buttons.add(clearButton);
+        buttons.add(foodButton);
+        buttons.add(infoButton);
+        buttons.add(exitButton);
+
+        return buttons;
     }
 }
